49673e926f30250f77b61e88aa3a6e873edc67f5
Donovan <donovan.de.kuiper@ordina.nl>
2024-04-12 12:34:00 +0200

    Created json serialisation plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,11 @@
 rootProject.name = "kuice"
 
 include("core")
+
+// Serialization Plugins
+include("plugins:serialization:json")
+project(":plugins:serialization:json").name = "serialization-json"
+
 // Authentication Plugins
 include("plugins:authentication:core")
 project(":plugins:authentication:core").name = "authentication-core"
