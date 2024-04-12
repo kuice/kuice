8b3e49ab009ab97bcd703645507ca5006e3aa57e
Donovan <donovan.de.kuiper@ordina.nl>
2024-04-12 12:32:18 +0200

    Create web socket plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 rootProject.name = "kuice"
 
 include("core")
+// Web Socket Plugins
+include("plugins:websocket:core")
+project(":plugins:websocket:core").name = "websocket-core"
 // Authentication Plugins
 include("plugins:authentication:core")
 project(":plugins:authentication:core").name = "authentication-core"
